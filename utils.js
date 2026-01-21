814c3bb916a263d9b1220be5396b29234007cec2
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2026-01-21 19:59:20 +0300

    Finishing Testing Arrays and Truthness

--- utils.js
+++ utils.js
@@ -5,8 +5,27 @@ const summ= (a,b)=>a+b;
 // Testing Strings
 const greeting = (name)=> `Hello ${name}`
 
+// ========================================
+// Testing Truthnes
+
+const isEven = (number)=>{
+    if(number %2 === 0)
+        return true;
+    else
+        return false;
+}
+
+
+// ========================================
+
+// Testing Arrays
+
+const Animals = ['cat', 'dog', 'monkey'];
+// ========================================
 
 module.exports = {
     summ,
-    greeting
+    greeting,
+    isEven,
+    Animals
 }
